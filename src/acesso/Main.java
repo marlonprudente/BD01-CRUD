363059e27d8538289539fa6752a185c501b9e70d
a1562339 <a1562339@localhost>
2015-11-25 11:01:53 -0200

    Adicionado Classes em persistence Feito Primeiro teste.

---
+++ src/acesso/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acesso;
+
+import entidades.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+/**
+ *
+ * @author a1562339
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("BD01CRUDPU");
+//2
+        EntityManager em = emf.createEntityManager();
+        Generos g = em.find(Generos.class, 2);
+//3
+        if (g != null) {
+            System.out.println(g.getGENERO_ID() + ", " + g.getDEESCRICAO());
+        } else {
+            System.out.println("Não encontrado!");
+        }
+    }
+}
