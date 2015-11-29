2eccc30f6731601b7c6219e9bca13e34bf1c2a8c
Jacichen <Jacichen@DAGUIDU>
2015-11-29 04:02:56 -0200

    teste

--- src/acesso/Main.java
+++ src/acesso/Main.java
@@ -7,7 +7,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class Main {
-
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("BD01CRUDPU");
         EntityManager em = emf.createEntityManager();
